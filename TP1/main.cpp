390607406c7def0fee07b806bca2e0c6a8ddfd71
Axel Dona <axel.dona@outlook.com>
2023-02-21 16:56:13 +0100

    quad

--- TP1/main.cpp
+++ TP1/main.cpp
@@ -3,6 +3,9 @@
 #include <glad/glad.h>
 #include <glimac/Program.hpp>
 #include <glimac/FilePath.hpp>
+#include <glimac/glm.hpp>
+#include <vector>
+#include <cstddef>
 
 int window_width  = 1280;
 int window_height = 720;
@@ -28,6 +31,23 @@ static void size_callback(GLFWwindow* /*window*/, int width, int height)
     window_width  = width;
     window_height = height;
 }
+
+class Vertex2DColor{
+
+
+public :
+
+    glm::vec2 position{};
+    glm::vec3 color{};
+
+
+    Vertex2DColor()=default;
+
+    Vertex2DColor(glm::vec2 posVec, glm::vec3 colorVec){
+        position = posVec;
+        color = colorVec;
+    }
+};
 
 int main(int, char** argv)
 {
@@ -78,11 +98,19 @@ int main(int, char** argv)
     glGenBuffers(1, &vbo); // on crée 1 vbo, son ID stocké dans l'unsigned int "vbo"
     glBindBuffer(GL_ARRAY_BUFFER, vbo); // pour pouvoir modifier notre VBO, on le bind à la cible GL_ARRAY_BUFFER (qui utilisée pour ça spécifiquement)
     // on stocke les coordonnées 2D des 3 vertex du triangle (soit 2 valeurs par vertice pour les coordonnées, soit 5 pour avoir la couleur avec)
-    GLfloat vertices[] = {-0.5f, -0.5f, 1.f, 0.f, 0.f, // premier sommet
-                          0.5f, -0.5f, 0.f, 1.f, 0.f, // deuxième sommet
-                          0.0f, 0.5f, 0.f, 0.f, 1.f // troisème sommet
+    //    GLfloat vertices[] = {-0.5f, -0.5f, 1.f, 0.f, 0.f, // premier sommet
+    //                          0.5f, -0.5f, 0.f, 1.f, 0.f, // deuxième sommet
+    //                          0.0f, 0.5f, 0.f, 0.f, 1.f // troisème sommet
+    //    };
+    std::vector<Vertex2DColor> vertices = {
+        Vertex2DColor(glm::vec2(-0.5, 0.5), glm::vec3(1, 0, 0)),
+        Vertex2DColor(glm::vec2(0.5, 0.5), glm::vec3(0, 1, 0)),
+        Vertex2DColor(glm::vec2(0.5, -0.5), glm::vec3(0, 0, 1)),
+        Vertex2DColor(glm::vec2(0.5, -0.5), glm::vec3(0, 0, 1)),
+        Vertex2DColor(glm::vec2(-0.5, -0.5), glm::vec3(0, 1, 0)),
+        Vertex2DColor(glm::vec2(-0.5, 0.5), glm::vec3(1, 0, 0))
     };
-    glBufferData(GL_ARRAY_BUFFER, 15*sizeof(GLfloat), vertices, GL_STATIC_DRAW); // on transmet les valeurs du tableau à la cible, GL_ARRAY_BUFFER. Le 2e paramètre est la taille du tableau en octets qui est le nombre de coordonnées * la taille d'un float (en général)
+    glBufferData(GL_ARRAY_BUFFER, vertices.size()*sizeof(Vertex2DColor)*2, vertices.data(), GL_STATIC_DRAW); // on transmet les valeurs du tableau à la cible, GL_ARRAY_BUFFER. Le 2e paramètre est la taille du tableau en octets qui est le nombre de coordonnées * la taille d'un float (en général)
     glBindBuffer(GL_ARRAY_BUFFER, 0); // on débind une fois nos modifications effectuées
 
     // Créer le VAO
@@ -94,10 +122,9 @@ int main(int, char** argv)
     glEnableVertexAttribArray(VERTEX_ATTR_POSITION); // 0 correspond par défaut à l'attribut position, ici on utilise 3 ce qui est précisé dans le fichier de shader
     glEnableVertexAttribArray(VERTEX_COLOR_POSITION);
     glBindBuffer(GL_ARRAY_BUFFER, vbo);
-    glVertexAttribPointer(VERTEX_ATTR_POSITION, 2, GL_FLOAT, GL_FALSE, 5*sizeof(GLfloat), 0); // ID de position, taille de l'attribut (2 pour les coordonnées), composants de type float, osef, stride qui est le nobre d'octets de l'attribut pour un sommet, pointeur qui est le décalage par rapport au début de l'attribut (ici 0 car première instance dans l'attribut)
-    glVertexAttribPointer(VERTEX_COLOR_POSITION, 3, GL_FLOAT, GL_FALSE, 5*sizeof(GLfloat), (const GLvoid*)(2*sizeof(GLfloat))); // ID position, taille (3 pour la couleur), ..., ..., stride identique car même attribut, pointeur différent car la couleur arrive après la position dans l'attribut, donc on met la taille de la position
+    glVertexAttribPointer(VERTEX_ATTR_POSITION, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex2DColor), (const GLvoid*)offsetof(Vertex2DColor, position)); // ID de position, taille de l'attribut (2 pour les coordonnées), composants de type float, osef, stride qui est le nobre d'octets de l'attribut pour un sommet, pointeur qui est le décalage par rapport au début de l'attribut (ici 0 car première instance dans l'attribut)
+    glVertexAttribPointer(VERTEX_COLOR_POSITION, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex2DColor), (const GLvoid*)offsetof(Vertex2DColor, color)); // ID position, taille (3 pour la couleur), ..., ..., stride identique car même attribut, pointeur différent car la couleur arrive après la position dans l'attribut, donc on met la taille de la position
     glBindBuffer(GL_ARRAY_BUFFER, 0);
-
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window)) {
@@ -105,7 +132,7 @@ int main(int, char** argv)
         glClear(GL_COLOR_BUFFER_BIT);
 
         /*Code de rendu ici*/
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawArrays(GL_TRIANGLES, 0, 6);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
